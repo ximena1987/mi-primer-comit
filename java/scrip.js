0b00915baa57397a6dd636b4e0f67dbd156ade9b
rebeca <ximenareberamirez@gmail.com>
2022-09-18 16:40:13 -0300

    mi primer commit clase 9

---
+++ java/scrip.js
@@ -0,0 +1,7 @@
+let nombre = prompt("ingrese su nombre:");
+document.write(nombre);
+let apellido = prompt("ingrese su apellido:");
+document.write(apellido);
+let nacionlidad = prompt("ingrese su nacionalidad:");
+document.write(nacionalidad)
+console.log(nombre, apellido, nacionalidad), alert(nombre_y_apellido);
